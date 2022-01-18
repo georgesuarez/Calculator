783ceffec6047085097df1d42c130101793f7fe7
George Suarez <georgesuarezdev@gmail.com>
2022-01-17 19:10:04 -0800

    Created the necessary buttons and text display

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculator.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Calculator
     {
         public MainWindow()
         {
+            CalculatorViewModel calculatorViewModel = new CalculatorViewModel();
             InitializeComponent();
         }
     }
---
+++ ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ViewModels
+{
+    internal class CalculatorViewModel
+    {
+        #region Fields
+        #endregion 
+
+        #region Ctor
+        public CalculatorViewModel()
+        {
+            _valueText = "0";
+        }
+        #endregion
+
+        #region Properties
+        private string _valueText;
+        public string valueText { get { return _valueText; } set { _valueText = value; } }
+        #endregion
+    }
+}
